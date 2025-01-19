245fc1fa58a384724331362b0d8012fdac3b09d6
Henrique O. Santos <202410486@uesb.edu.br>
2025-01-19 12:35:30 -0300

    acionamento do buzzer

--- comunicacao-serial-microcontrolador.c
+++ comunicacao-serial-microcontrolador.c
@@ -1,15 +1,43 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include <string.h>
+
+#define BUZZER 21
+
+void initBuzzer(){
+    gpio_init(BUZZER);
+    gpio_set_dir(BUZZER, GPIO_OUT);
+}
+
+void buzzer_on(uint buzzer_pin, float frequency, uint duration_ms){
+    float period = 1.0f / frequency;                        // Período da onda quadrada
+    float half_period = period / 2;                         // Meio período em segundos
+    uint cycles = (uint)((duration_ms / 1000.0f) / period); // Número de ciclos
+
+    for (uint i = 0; i < cycles; i++)
+    {
+        gpio_put(buzzer_pin, 1);            // Seta o pino como HIGH
+        sleep_us((int)(half_period * 1e6)); // Espera pelo meio período
+        gpio_put(buzzer_pin, 0);            // Seta o pino como LOW
+        sleep_us((int)(half_period * 1e6)); // Espera pelo meio período
+    }
+}
 
 
 
-
-int main()
-{
+int main(){
+    char comando[32];
+    stdio_init_all();
+    initBuzzer();
    
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        scanf("%s", comando);
+        
+        // ACIONA O BUZZER POR 2 SEGUNDOS
+        if(strcmp(comando, "BUZZER") == 0 || strcmp(comando, "buzzer") == 0){
+            buzzer_on(21, 440, 2000);
+            printf("BUZZER ACIONADO!\n");
+        }
     }
 }
